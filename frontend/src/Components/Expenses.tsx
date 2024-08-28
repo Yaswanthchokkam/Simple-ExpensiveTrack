b9e462d1145906e11007cded2edc44b52bf67258
Your Name <yaswanthchokkam2002@gmail.com>
2024-08-28 13:24:36 +0530

    update Expenses

--- frontend/src/Components/Expenses.tsx
+++ frontend/src/Components/Expenses.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 interface ExpenseData {
   _id: string;
   itemId: number;
